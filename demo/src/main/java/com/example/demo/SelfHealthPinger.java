f1dacdd6ab31ffbcde43ca8a1fcfabcc1b8e7b74
Kris De Volder <kdevolder@pivotal.io>
2022-07-01 22:13:07 -0700

    Restructure maven build
    - add parent/aggregator
    - mvn script bits in parent

--- demo/src/main/java/com/example/demo/SelfHealthPinger.java
+++ demo/src/main/java/com/example/demo/SelfHealthPinger.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -25,14 +28,20 @@ public class SelfHealthPinger {
 	
 	@Scheduled(fixedRate = 5000)
 	void selfHealthCheck() throws Exception {
-		Object h = client.health();
-		System.out.println("Health = "+mapper.writeValueAsString(h));
-
-		Object ph = client.healthForPath("ping");
-		System.out.println("Health.ping = "+mapper.writeValueAsString(ph));
+//		Object h = client.health();
+//		System.out.println("Health = "+mapper.writeValueAsString(h));
+//
+//		Object ph = client.healthForPath("ping");
+//		System.out.println("Health.ping = "+mapper.writeValueAsString(ph));
+//		
+//		Object md = client.getEndpointMetadata();
+//		System.out.println("ops = "+mapper.writeValueAsString(md));
 		
-		Object md = client.getEndpointMetadata();
-		System.out.println("ops = "+mapper.writeValueAsString(md));
+		Object metric = client.call("metrics.metric", Map.of(
+				"requiredMetricName", "jvm.memory.used",
+				"tag", List.of("area:heap")
+		));
+		System.out.println("metric = "+mapper.writeValueAsString(metric));
 
 	}
 	
