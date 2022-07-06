fb127aadb1ee568d8c252b8f9688ca4985b1ef3a
Kris De Volder <kdevolder@pivotal.io>
2022-07-06 10:02:58 -0700

    Create placeholder project for 'rsocktuator-controller' PoC

---
+++ rsocktuator-controller/src/main/java/com/example/demo/RsocktuatorControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocktuatorControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocktuatorControllerApplication.class, args);
+	}
+
+}
